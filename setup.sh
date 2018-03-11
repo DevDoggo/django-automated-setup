6b5c1d104e62ccbdfde696eb2181936bae3dca48
DevDoggo <devdoggo@protonmail.com>
2018-03-11 20:25:46 +0100

    Have started to shape views/urls according to creation

--- setup.sh
+++ setup.sh
@@ -30,3 +30,19 @@ read appname
 django-admin startproject $projectname
 cd $projectname
 python3 manage.py startapp $appname
+
+
+cd ..
+#Places Static and Template folders into app
+cp -r static $projectname/$appname/static
+cp -r templates $projectname/$appname/templates
+
+
+#Modify views/urls/settings to route correctly
+sed -i "s/appname/$appname/g" misc-files/main_urls.py
+
+#Place views/urls files
+cp misc-files/main_urls.py $projectname/$projectname/urls.py
+cp misc-files/app_urls.py $projectname/$appname/urls.py
+cp misc-files/app_views.py $projectname/$appname/views.py
+
