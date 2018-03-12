2d39743ed69e2d7f6905f6fd652d86a53c0fea15
DevDoggo <devdoggo@protonmail.com>
2018-03-12 19:15:10 +0100

    Added a run.sh to the django project

--- setup.sh
+++ setup.sh
@@ -29,8 +29,10 @@ echo -e ">>> Set django 'projectname':"
 read projectname
 echo -e ">>> Set django 'appname':"
 read appname
-echo -e ">>> Set django 'local ip': (ex, 0.0.0.0)"
+echo -e ">>> Set django 'local ip': (ex. 0.0.0.0)"
 read localip
+echo -e ">>> Set django 'port': (ex. 8000)"
+read port
 
 #django project and app creation
 django-admin startproject $projectname
@@ -40,20 +42,20 @@ python3 manage.py startapp $appname
 cd .. #brings us back up to document root
 echo -e "--Setup: Configuring local files\n" 
 #Places Static and Template folders into app
-cp -r static $projectname/$appname/static
-cp -r templates $projectname/$appname/templates
+cp -r templfiles/static $projectname/$appname/static
+cp -r templfiles/templates $projectname/$appname/templates
 
 #Place views/urls files
-cp misc-files/main_urls.py $projectname/$projectname/urls.py
-cp misc-files/app_urls.py $projectname/$appname/urls.py
-cp misc-files/app_views.py $projectname/$appname/views.py
+cp templfiles/misc-files/main_urls.py $projectname/$projectname/urls.py
+cp templfiles/misc-files/app_urls.py $projectname/$appname/urls.py
+cp templfiles/misc-files/app_views.py $projectname/$appname/views.py
 
 #Modify views/urls/settings to route correctly
 sed -i "s/appname/$appname/g" $projectname/$projectname/urls.py 
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$localip\'\]/g" $projectname/$projectname/settings.py
 sed -i "s/'DIRS'\: \[\]/'DIRS'\: \[\'$appname\/templates\'\]/g" $projectname/$projectname/settings.py
 
-cat misc-files/static-dir-code >> $projectname/$projectname/settings.py
+cat templfiles/misc-files/static-dir-code >> $projectname/$projectname/settings.py
 sed -i "s/appname_example/$appname/g" $projectname/$projectname/settings.py
 
 #Django migration
@@ -62,8 +64,9 @@ echo -e "--Setup: Django Migration...\n"
 #python3 manage.py makemigrations
 python3 manage.py migrate
 
-
-
+#Creating Run File
+echo "#!/bin/bash" > run.sh
+echo "python3 manage.py runserver $localip:$port" >> run.sh
 
 
 #Initialization Complete
